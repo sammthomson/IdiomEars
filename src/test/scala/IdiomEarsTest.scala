acfa76b2ab1457508ffb4b3b8ed816cfa6279f48
Sam Thomson <sammthomson@gmail.com>
2016-03-30 17:55:55 -0700

    test that nesting works

--- src/test/scala/IdiomEarsTest.scala
+++ src/test/scala/IdiomEarsTest.scala
@@ -7,11 +7,14 @@ import org.scalatest.{Matchers, FlatSpec}
 
 import scala.language.postfixOps
 
+
 class IdiomEarsTest extends FlatSpec with Matchers with GeneratorDrivenPropertyChecks {
+  val f = (a: Int) => (b: Int) => a * b
+  val g = (a: Int) => (b: Int) => (s: String) => s + (a * b)
+
   behavior of "⊏| ... |⊐"
 
   it should "work for binary functions and Lists" in {
-    val f = (a: Int) => (b: Int) => a * b
     forAll { (a: List[Int], b: List[Int]) =>
       val result = ⊏| (f) (a) (b) |⊐
       val expected = Applicative[List].pure(f).ap(a).ap(b)
@@ -20,10 +23,29 @@ class IdiomEarsTest extends FlatSpec with Matchers with GeneratorDrivenPropertyC
   }
 
   it should "work for ternary functions and Options" in {
-    val g = (a: Int) => (b: Int) => (s: String) => s + (a * b)
     forAll { (a: Option[Int], b: Option[Int], s: Option[String]) =>
       val result = *| (g) (a) (b) (s) |*
       val expected = Applicative[Option].pure(g).ap(a).ap(b).ap(s)
+      result should equal (expected)
+    }
+  }
+
+  it should "nest" in {
+    val F = Applicative[Option]
+    forAll { (a: Option[Int], b: Option[Int]) =>
+      val result = (  // ouch my eyes
+          ⊏|
+            (f)
+            (a)
+            (⊏| (f) (a) (b) |⊐)
+          |⊐
+        )
+      val expected =
+        F.pure(f)
+            .ap(a)
+            .ap(
+              F.pure(f).ap(a).ap(b)
+            )
       result should equal (expected)
     }
   }
